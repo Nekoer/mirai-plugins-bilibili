73fbe2ab6626b425ba015966db5c670460b82355
Nekoer <243462032@qq.com>
2022-04-27 14:05:56 +0800

    update LICENSE

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+
+
     id("net.mamoe.mirai-console") version "2.11.0-M2.2"
 }
 
@@ -15,7 +20,7 @@ repositories {
 
 dependencies {
 
-    implementation("com.alibaba:fastjson:1.2.80")
+    implementation("com.alibaba:fastjson:2.0.1")
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("org.apache.httpcomponents:httpclient:4.5.13")
 
@@ -42,4 +47,89 @@ tasks.test {
 // config JVM target to 1.8 for kotlin compilation tasks
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.register<Zip>("stuffZip") {
+    archiveBaseName.set("stuff")
+    from("src/stuff")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = rootProject.name
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set(rootProject.name)
+                description.set("The Robot Kotlin SDK on Tencent Channel")
+                url.set("https://github.com/Nekoer/mirai-plugins-bilibili")
+//                properties.set(mapOf(
+//                    "myProp" to "value",
+//                    "prop.with.dots" to "anotherValue"
+//                ))
+                licenses {
+                    license {
+                        name.set("GNU AFFERO GENERAL PUBLIC LICENSE, Version 3.0")
+                        url.set("https://www.gnu.org/licenses/agpl-3.0.en.html")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Nekoer")
+                        name.set("Nekoer")
+                        email.set("hcyacg@vip.qq.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Nekoer/mirai-plugins-bilibili.git")
+                    developerConnection.set("scm:git:ssh://github.com/Nekoer/mirai-plugins-bilibili.git")
+                    url.set("https://github.com/Nekoer/mirai-plugins-bilibili")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri(layout.buildDirectory.dir("repos/releases"))
+            val snapshotsRepoUrl = uri(layout.buildDirectory.dir("repos/snapshots"))
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
+            username.set(properties["myNexusTokenUsername"].toString())
+            password.set(properties["myNexusTokenPassword"].toString())
+        }
+    }
 }
